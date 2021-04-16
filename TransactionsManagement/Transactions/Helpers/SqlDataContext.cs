1c1828eb3be7ccff5d8be0da489952fcd494ed66
Kgaugelo Molema <kmolema@gmail.com>
2021-04-16 13:38:15 +0200

    Add EF

---
+++ TransactionsManagement/Transactions/Helpers/SqlDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Transactions.Models;
+
+namespace Transactions.Helpers
+{
+    public class SqlDataContext: DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public SqlDataContext(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sqlite database
+            options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<TransactionsModel> Transactions { get; set; }
+    }
+}
